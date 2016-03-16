fba855697b4a6d8dcee829275fef9b3c796059f4
Jordan Meier and nic netzel <netzeln@gmail.com>
2016-03-16 14:31:42 -0700

    add locateAddress function

--- js/map.js
+++ js/map.js
@@ -9,7 +9,7 @@ exports.initMap = function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
-}
+};
 
 
 exports.findLocation = function(lat, lon){
@@ -22,31 +22,32 @@ exports.findLocation = function(lat, lon){
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
-  // var lat = lat;
-  // var lon = lon;
-  // console.log(lat);
-  // var mapProp = {
-  //   center:new google.maps.LatLng(lat,lon),
-  //   zoom:15,
-  //   mapTypeControl: false,
-  //   mapTypeId: google.maps.MapTypeId.ROADMAP
-  // };
-  // map = new google.maps.Map(document.getElementById('map'), {
-  //   center: {lat: lat, lng: -lon},
-  //   zoom: 15
-  // });
-}
+};
 
-// exports.locateUser = function() {
-//   // If the browser supports the Geolocation API
-//   if (navigator.geolocation){
-//     var positionOptions = {
-//       enableHighAccuracy: true,
-//       timeout: 10 * 1000 // 10 seconds
-//     };
-//     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
-//   }
-//   else {
-//     alert("Your browser doesn't support the Geolocation API");
-//   }
-// }
+exports.locateAddress = function(address) {
+  var geocoder =  new google.maps.Geocoder();
+  var latitude = 0;
+  var longitude = 0;
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      latitude = results[0].geometry.location.lat();
+      longitude = results[0].geometry.location.lng();
+      console.log(latitude);
+      exports.findLocation(latitude, longitude);
+    } else {
+      alert("Something went wrong " + status);
+    }
+  });
+};
+
+exports.findMirror = function(lat, lon){
+  var lat = lat - (lat * 2);
+  var lon = lon - (lon * 2);
+  var mapProp = {
+    center:new google.maps.LatLng(lat,lon),
+    zoom:10,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
+};
