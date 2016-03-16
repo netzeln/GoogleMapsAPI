b61db539a4475a6a4733c395f15aa45c3a700837
Jordan Meier and nic netzel <netzeln@gmail.com>
2016-03-16 13:34:07 -0700

    include require statements for both functions being used in broswer=interface.js

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,6 @@
 // var apiKey = require('./../.env').apiKey;
 var initMap = require('./../js/map.js').initMap;
-// var findLocation = require('./../js/map.js').findLocation;
+var findLocation = require('./../js/map.js').findLocation;
 
 $(document).ready(function(){
   google.maps.event.addDomListener(window, 'load', initMap);
@@ -8,10 +8,12 @@ $(document).ready(function(){
   // $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap").then(function(response) {
   //
   //   };
-  // $('#getCoord').click(function(){
-  //   var lat = $('input#getLat').val();
-  //   var lon = $('input#getLon').val();
-  //   var locate = findLocation(lat, lon);
-  //   console.log(lat);
-  // });
+  $('#getCoord').click(function(){
+    var lat = $('input#getLat').val();
+    console.log(lat);
+    var lon = $('input#getLon').val();
+    console.log(lon);
+    var locate = findLocation(lat, lon);
+    google.maps.event.addDomListener(window, 'load', locate);
+  });
 });
