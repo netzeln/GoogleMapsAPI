891882b886139de958267b812dd188f9ac3256fb
Jordan Meier and nic netzel <jbirdmeier@gmail.com>
2016-03-16 13:33:29 -0700

    reformat findLocation function and add export flags to functions in map.js

--- js/map.js
+++ js/map.js
@@ -1,28 +1,52 @@
 
 exports.initMap = function() {
+  var lat = 45.519685;
+  var lon = -122.6777623;
   var mapProp = {
-    center:new google.maps.LatLng(45.519685,-122.6777623),
+    center:new google.maps.LatLng(lat,lon),
     zoom:15,
     mapTypeControl: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("map"),mapProp);
 }
-  // var map;
-//   console.log("in initMap");
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 45.519685, lng: -122.6777623},
-//     zoom: 15
-//   });
-//   console.log(map);
-// }
 
-// exports.findLocation = function(lat, lon){
-//   var map2;
-//   var lat = lat;
-//   var lon = lon;
-//   map2 = new google.maps.Map(document.getElementById('map2'), {
-//     center: {lat: lat, lng: -lon},
-//     zoom: 15
-//   });
+
+exports.findLocation = function(lat, lon){
+  var lat = lat;
+  var lon = lon;
+  var mapProp = {
+    center:new google.maps.LatLng(lat,lon),
+    zoom:10,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
+  // var lat = lat;
+  // var lon = lon;
+  // console.log(lat);
+  // var mapProp = {
+  //   center:new google.maps.LatLng(lat,lon),
+  //   zoom:15,
+  //   mapTypeControl: false,
+  //   mapTypeId: google.maps.MapTypeId.ROADMAP
+  // };
+  // map = new google.maps.Map(document.getElementById('map'), {
+  //   center: {lat: lat, lng: -lon},
+  //   zoom: 15
+  // });
+}
+
+// exports.locateUser = function() {
+//   // If the browser supports the Geolocation API
+//   if (navigator.geolocation){
+//     var positionOptions = {
+//       enableHighAccuracy: true,
+//       timeout: 10 * 1000 // 10 seconds
+//     };
+//     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+//   }
+//   else {
+//     alert("Your browser doesn't support the Geolocation API");
+//   }
 // }
