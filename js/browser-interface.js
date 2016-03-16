2638124bcd7d0b1689375b524a992fe342a9b752
Jordan Meier and nic netzel <jbirdmeier@gmail.com>
2016-03-16 11:41:44 -0700

    add export to our mapInit function so that it can be called on in browser-interface

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,11 +1,17 @@
 // var apiKey = require('./../.env').apiKey;
-// var initMap = require('./../js/map.js').initMap;
+var initMap = require('./../js/map.js').initMap;
+// var findLocation = require('./../js/map.js').findLocation;
 
 $(document).ready(function(){
-  // initMap();
+  google.maps.event.addDomListener(window, 'load', initMap);
 
   // $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap").then(function(response) {
   //
   //   };
-
-  });
+  // $('#getCoord').click(function(){
+  //   var lat = $('input#getLat').val();
+  //   var lon = $('input#getLon').val();
+  //   var locate = findLocation(lat, lon);
+  //   console.log(lat);
+  // });
+});
--- js/map.js
+++ js/map.js
@@ -1,24 +1,28 @@
-// initMap = function(){
-// console.log("in initMap function");
-// var mapProp = {
-//   center: new google.maps.LatLng(45.519685, 122.6777623),
-//   zoom: 15,
-//   mapTypeId:google.maps.MapTypeId.ROADMAP
-// };
-//
-//  var map = new google.maps.Map(document.getElementById('map'), mapProp);
-//   // var map;
-//   // map = new google.maps.Map(document.getElementById('map'), {
-//   //   center: {lat: 45.519685 , lng:-122.6777623},
-//   //   zoom: 15
-//   // });
-// };
-var map;
-function initMap() {
-  console.log("in initMap");
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-  console.log(map);
+
+exports.initMap = function() {
+  var mapProp = {
+    center:new google.maps.LatLng(45.519685,-122.6777623),
+    zoom:15,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("map"),mapProp);
 }
+  // var map;
+//   console.log("in initMap");
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 45.519685, lng: -122.6777623},
+//     zoom: 15
+//   });
+//   console.log(map);
+// }
+
+// exports.findLocation = function(lat, lon){
+//   var map2;
+//   var lat = lat;
+//   var lon = lon;
+//   map2 = new google.maps.Map(document.getElementById('map2'), {
+//     center: {lat: lat, lng: -lon},
+//     zoom: 15
+//   });
+// }
