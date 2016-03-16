47e9d48759c903c0e2d26c91556c21e751c48205
Jordan Meier and nic netzel <jbirdmeier@gmail.com>
2016-03-16 14:32:15 -0700

    add a click listener for get address form

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,19 +1,36 @@
 // var apiKey = require('./../.env').apiKey;
 var initMap = require('./../js/map.js').initMap;
 var findLocation = require('./../js/map.js').findLocation;
+var findMirror = require('./../js/map.js').findMirror;
+var locateAddress = require('./../js/map.js').locateAddress;
 
 $(document).ready(function(){
   google.maps.event.addDomListener(window, 'load', initMap);
 
-  // $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap").then(function(response) {
-  //
-  //   };
+
   $('#getCoord').click(function(){
     var lat = $('input#getLat').val();
     console.log(lat);
     var lon = $('input#getLon').val();
     console.log(lon);
     var locate = findLocation(lat, lon);
+    google.maps.event.addDomListener(window, 'load', locate);
+  });
+
+  $('#getAddress').click(function(){
+    var address = $('input#Address').val();
+    var locate = locateAddress(address);
+    google.maps.event.addDomListener(window, 'load', locate);
+  });
+
+
+
+  $('#getMirror').click(function(){
+    var lat = $('input#getLat').val();
+    console.log(lat);
+    var lon = $('input#getLon').val();
+    console.log(lon);
+    var locate = findMirror(lat, lon);
     google.maps.event.addDomListener(window, 'load', locate);
   });
 });
