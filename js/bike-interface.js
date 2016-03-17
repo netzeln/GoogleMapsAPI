6ff98ac9edd159502ee798557857646f69932e24
Jordan Meier and Nic Netzel <netzeln@gmail.com>
2016-03-17 14:51:37 -0700

    add jQuery to hide and show bike div

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -7,7 +7,7 @@ var locateAddress = require('./../js/map.js').locateAddress;
 var addSearchBox = require('./../js/map.js').addSearchBox;
 
 $(document).ready(function(){
-
+  $('#bikeThing').hide();
   $('#getBikeThing').click(function(){
     var lat = $('input#getLat').val();
 
@@ -16,6 +16,7 @@ $(document).ready(function(){
 
   $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=" + lat + "%2C%20"+ lon + "&proximity_square=10&access_token=1f75ed78caee48983b97877d1d31d08aa806261127f18ca68f48f4863f3b3c5c").then(function(response) {
     // response.bikes.date_stolen.sort();
+    $('#bikeThing').show();
     for (var i =0; i <= response.bikes.length; i++){
       var date = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
       $('#bikeName').append("<li> a " + response.bikes[i].frame_colors + " " + response.bikes[i].title + " stolen from " + response.bikes[i].stolen_location + " on " + date + "</li>");
