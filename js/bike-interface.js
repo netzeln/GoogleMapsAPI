49aa4cc34e42c77410094fe41d24acf65c8f4800
Jordan Meier and Nic Netzel <jbirdmeier@comcast.net>
2016-03-17 14:43:42 -0700

    add a for loop to sort through all bikes returned in area and display various attributes

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -12,10 +12,14 @@ $(document).ready(function(){
     var lat = $('input#getLat').val();
 
     var lon = $('input#getLon').val();
+    $("#bikeName").empty();
 
-  $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=" + lat + "%2C%20"+ lon + "&proximity_square=100&access_token=1f75ed78caee48983b97877d1d31d08aa806261127f18ca68f48f4863f3b3c5c").then(function(response) {
-
-    $('#bikeName').text(response.bikes[0].title);
+  $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=" + lat + "%2C%20"+ lon + "&proximity_square=10&access_token=1f75ed78caee48983b97877d1d31d08aa806261127f18ca68f48f4863f3b3c5c").then(function(response) {
+    // response.bikes.date_stolen.sort();
+    for (var i =0; i <= response.bikes.length; i++){
+      var date = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
+      $('#bikeName').append("<li> a " + response.bikes[i].frame_colors + " " + response.bikes[i].title + " stolen from " + response.bikes[i].stolen_location + " on " + date + "</li>");
+    }
   });
   // console.log(bikes);
     // $('#search').prepend('<input  id="pac-input" class="controls" type="text" placeholder="Search Box">');
