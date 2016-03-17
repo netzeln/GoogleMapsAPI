3e6e3360550b64a327a0220cf0d621d9fa01de4e
Jordan Meier and Nic Netzel <jbirdmeier@comcast.net>
2016-03-17 15:04:43 -0700

    add a sort method to sort the bike array by date

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -15,7 +15,9 @@ $(document).ready(function(){
     $("#bikeName").empty();
 
   $.get("https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=" + lat + "%2C%20"+ lon + "&proximity_square=10&access_token=1f75ed78caee48983b97877d1d31d08aa806261127f18ca68f48f4863f3b3c5c").then(function(response) {
-    // response.bikes.date_stolen.sort();
+    response.bikes.sort(function(a,b){
+      return new Date(b.date_stolen) - new Date(a.date_stolen);
+    });
     $('#bikeThing').show();
     for (var i =0; i <= response.bikes.length; i++){
       var date = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
